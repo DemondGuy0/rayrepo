9f0fa8372eff2e7293a3b748bcde5c5ee2a46f28
DemondGuy0 <103087144+DemondGuy0@users.noreply.github.com>
2022-11-25 02:45:16 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID =  '17158127' #Tu api id de telegram
 API_HASH = 'd1a5974070430293b64fde5339591447' #Tu api id de telegram
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','reymichel2009').split(';')
+ACCES_USERS = os.environ.get('tl_admin_user','killerj00').split(';')
 
 static_proxy = 'socks5h://KGGJJJYIJFLIFEYDJFLEKHYGCHDHRHHKCHHGCE'
 PROXY = ProxyCloud.parse(static_proxy)
